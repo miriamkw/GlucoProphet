3b316c3c1aceb43c94a0b5716bdcdfb87921ffb2
Miriam K. Wolff <miriamkwolff@hotmail.com>
2023-10-15 18:19:46 +0200

    Initial commit

---
+++ GlucoProphet/GlucoProphetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlucoProphetApp.swift
+//  GlucoProphet
+//
+//  Created by Miriam K. Wolff on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GlucoProphetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
